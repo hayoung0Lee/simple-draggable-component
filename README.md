f22b9852004aab90588545dda4ac3a1cf3492cce
hayoung0Lee <hayoung0.lee@gmail.com>
2022-03-02 21:07:33 -0600

    Draggable 위치 업데이트 구현

--- README.md
+++ README.md
@@ -2,11 +2,21 @@
 
 - Create a simple React project
 - Create <Box> components as you want.
-- Create <Draggable> component making <Box> component draggable without an external library.
+- Create <Draggable> component making <Box> component draggable without an
+  external library.
+
 ```
 <Draggable>
 	<Box />
 </Draggable>
 ```
-Make Draggable area cannot overflow current window.
-There is an additional score with using TypeScript and writing unit test.
+
+Make Draggable area cannot overflow current window. There is an additional score
+with using TypeScript and writing unit test.
+
+## 참고한 문서
+
+- https://web.dev/drag-and-drop/
+- https://www.smashingmagazine.com/2020/02/html-drag-drop-api-react/
+- getBoundingClientRect:
+  https://developer.mozilla.org/en-US/docs/Web/API/Element/getBoundingClientRect
--- src/components/Draggable.tsx
+++ src/components/Draggable.tsx
@@ -1,9 +1,37 @@
+import { useEffect, useRef, useState } from "react";
+
 interface Props {
     children: JSX.Element[] | JSX.Element
 }
 
+interface PositionType {
+    x: number,
+    y: number
+}
+
 const Draggable = ({ children }: Props) => {
-    return <div className="draggable">
+    const ref = useRef<HTMLDivElement>(null);
+    const [positionXY, setStatePositionXY] = useState<PositionType>({ x: 0, y: 0 })
+    const positionRef = useRef<PositionType>(positionXY);
+
+    return <div
+        ref={ref}
+        className="draggable"
+        draggable="true"
+        style={{
+            // 첫번째 인자, 가로방향: 왼쪽에서 오른쪽으로 커지는 방향
+            // 두번째 인자, 세로방향: 위에서 아래로 커지는 방향
+            transform: `translate(${positionXY.x}px, ${positionXY.y}px)`
+        }}
+        onDragStart={(e) => {
+            positionRef.current = { x: e.clientX, y: e.clientY }
+        }}
+        onDragEnd={(e) => {
+            const diffX = e.clientX - positionRef.current.x;
+            const diffY = e.clientY - positionRef.current.y;
+            setStatePositionXY({ x: positionXY.x + diffX, y: positionXY.y + diffY })
+        }}
+    >
         {children}
     </div>
 }
